4a0725ac98928f4e3674b75931c57aaae14f876f
sonhoaian <an.son@lgvn14368-w7.logigear.com>
2018-02-06 15:40:55 +0700

    update file name

---
+++ Common/common/Utilities.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package common;
+
+/**
+ * @author an.son
+ *
+ */
+public class Utilities {
+	
+	//locate project path
+	public static String getProjectPath() {
+		return System.getProperty("user.dir");
+	}
+}
